a69ab69bc1c568a7f90c47a791e3401001035eda
Ray <raphee.rattanamanoonporn@gmail.com>
2026-07-23 06:46:42 +0700

    test8

---
+++ midterm/exam1.py
@@ -0,0 +1,29 @@
+import cv2 as cv
+import os
+
+xml_path = "C:\\Users\\SPSM LAB 7\\Documents\\024329\\computer_vision-main\\haarcascade_frontalface_default.xml"
+
+detected = 0
+
+for i in range (1,121):
+	if i < 10:
+		img = cv.imread(f"C:\\Users\\SPSM LAB 7\\Documents\\024329\\computer_vision-main\\faces\\img_00{i}.jpg")
+	elif i <100:
+		img = cv.imread(f"C:\\Users\\SPSM LAB 7\\Documents\\024329\\computer_vision-main\\faces\\img_0{i}.jpg")
+	else:
+		img = cv.imread(f"C:\\Users\\SPSM LAB 7\\Documents\\024329\\computer_vision-main\\faces\\img_{i}.jpg")
+	face_cascde = cv.CascadeClassifier(xml_path)
+	gray_img = cv.cvtColor(img, cv.COLOR_RGB2GRAY)
+	scale = 1.9
+	minNeighbor = 1
+	face_detect = face_cascde.detectMultiScale(gray_img, scale, minNeighbor)
+	if len(face_detect) > 0:
+    		for x, y, w, h in face_detect:
+       			cv.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    		cv.imwrite(f"C:\\Users\\SPSM LAB 7\\Documents\\024329\\computer_vision-main\\output\\img{i}.jpg", img)
+	else:
+    	    	print(f"No faces detected. {i}")
+	num = len(face_detect)
+	detected += num
+
+print(f"detected: {detected} faces")
